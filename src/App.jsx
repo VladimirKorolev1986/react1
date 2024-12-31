440b63c86a845be30e169c40ba870e8ab1becff8
VladimirKorolev <theing1986@gmail.com>
2024-12-31 17:19:12 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
+
+function App() {
+  return (
+    <>
+      <h1>Какой то заголовок</h1>
+      <p>Какой то текст</p>
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/JournalItem/JournalItem.jsx
@@ -0,0 +1,13 @@
+import "./JournalItem.css";
+
+function JournalItem() {
+  <div className="journal-item">
+    <h2 className="journal-item__header">1</h2>
+    <h2 className="journal-item__body">
+      <div className="journal-item__date">2</div>
+      <div className="journal-item__text">3</div>
+    </h2>
+  </div>;
+}
+
+export default JournalItem;
