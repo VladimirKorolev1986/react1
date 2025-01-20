746c38f8411e883ac73851ca2e1403bfaaa37336
VladimirKorolev <theing1986@gmail.com>
2025-01-20 21:20:38 +0300

    fix date

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,71 @@
-import React, { useState } from "react";
-import "./App.css";
-import Button from "./components/Button/Button";
-import JournalItem from "./components/JournalItem/JournalItem";
-import CardButton from "./components/CardButton/CardButton";
-import LeftPanel from "./layouts/LeftPanel/LeftPanel";
-import Body from "./layouts/Body/Body";
-import Header from "./components/Header/Header";
-import JournalList from "./components/JournalList/JournalList";
-import JournalAddButton from "./components/JournalAddButton/JournalAddButton";
-import JournalForm from "./components/JournalForm/JournalForm";
-import { id } from "date-fns/locale";
+import { useState } from 'react';
+import './App.css';
+import CardButton from './components/CardButton/CardButton';
+import Header from './components/Header/Header';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
+import JournalForm from './components/JournalForm/JournalForm';
+import JournalItem from './components/JournalItem/JournalItem';
+import JournalList from './components/JournalList/JournalList';
+import Body from './layouts/Body/Body';
+import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
 const INITIAL_DATA = [
-  {
-    id: 1,
-    title: "Подготовка к обновлению курсов",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, animi! Rerum aut voluptate eveniet quidem reprehenderit quas laboriosam! Perspiciatis pariatur velit autem explicabo debitis non quibusdam distinctio sequi nesciunt consequuntur.",
-    date: new Date(),
-  },
-  {
-    id: 2,
-    title: "Поход в горы",
-    text: "Думал что очень много времени",
-    date: new Date(),
-  },
+	{
+		id: 1,
+		title: 'Подготовка к обновлению курсов',
+		text: 'Горные походы открывают удивительные природные ландшафт',
+		date: new Date()
+	},
+	{
+		id: 2,
+		title: 'Поход в годы',
+		text: 'Думал, что очень много времени',
+		date: new Date()
+	}
 ];
 
 function App() {
-  const [items, setItems] = useState(INITIAL_DATA)
+	const [items, setItems] = useState(INITIAL_DATA);
 
-  const addItem = (item) => {
-    setItems(oldItems => [...oldItems, {
-      text: item.text,
-      title: item.title,
-      date: new Date(item.date),
-      id: Math.max(...oldItems.map(i => i.id)) + 1
-    }])
-  }
-  const sortItems = (a, b) => {
-    if (a.date < b.date) {
-      return 1
-    } else {
-      return -1
-    }
-  }
+	const addItem = item => {
+		setItems(oldItems => [...oldItems, {
+			text: item.text,
+			title: item.title,
+			date: new Date(item.date),
+			id: Math.max(...oldItems.map(i => i.id)) + 1
+		}]);
+	};
 
+	const sortItems = (a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		} else {
+			return -1;
+		}
+	};
 
-
-  return (
-    <div className="app">
-      <LeftPanel>
-        <Header />
-        <JournalAddButton />
-        <JournalList>
-          {items.sort(sortItems).map(el=>(
-            <CardButton key={el.id}>
-            <JournalItem
-              title={el.title}
-              text={el.text}
-              data={el.date}
-            />
-          </CardButton>
-          ))}
-          
-
-        </JournalList>
-      </LeftPanel>
-      <Body>
-        <JournalForm onSubmit={addItem}/>
-      </Body>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<LeftPanel>
+				<Header/>
+				<JournalAddButton/>
+				<JournalList>
+					{items.sort(sortItems).map(el => (
+						<CardButton key={el.id}>
+							<JournalItem 
+								title={el.title}
+								text={el.text}
+								date={el.date}
+							/>
+						</CardButton>
+					))}
+				</JournalList>
+			</LeftPanel>
+			<Body>
+				<JournalForm onSubmit={addItem}/>
+			</Body>
+		</div>
+	);
 }
 
 export default App;
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,30 +1,23 @@
-import Button from "../Button/Button";
-import "./JournalForm.css";
-import { useState } from "react";
+import './JournalForm.css';
+import Button from '../Button/Button';
 
-function JournalForm({onSubmit}) {
+function JournalForm({ onSubmit }) {
+	const addJournalItem = (e) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const formProps = Object.fromEntries(formData);
+		onSubmit(formProps);
+	};
 
-  const addJornalItem = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formProps = Object.fromEntries(formData);
-    onSubmit(formProps);
-  };
-
-  return (
-    <form className="journal-form" onSubmit={addJornalItem}>
-      <input type="text" name="title" />
-      <input type="date" name="date" />
-      <input type="text" name="tag"  />
-      <textarea name="text" id="" cols="30" rows="10"></textarea>
-      <Button
-        text={"Сохранить"}
-        onClick={() => {
-          console.log("Нажали");
-        }}
-      />
-    </form>
-  );
+	return (
+		<form className='journal-form' onSubmit={addJournalItem}>
+			<input type='text' name='title'/>
+			<input type='date' name='date' />
+			<input type='text' name='tag' />
+			<textarea name="post" id="" cols="30" rows="10"></textarea>
+			<Button text="Сохранить" onClick={() => { console.log('Нажали');}} />
+		</form>
+	);
 }
 
 export default JournalForm;
